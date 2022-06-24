8bef5a47acbc8011ce1f901e4bd3b6465e9ade0c
akdlsz21 <81629070+akdlsz21@users.noreply.github.com>
2022-06-24 20:38:08 +0900

    Build Test

--- pages/index.tsx
+++ pages/index.tsx
@@ -16,7 +16,7 @@ const Home: NextPage = () => {
 
 			<main className={styles.main}>
 				<h1 className={styles.title}>
-					Welcome to <a href="https://nextjs.org">Next.js!</a>
+					Welcome E <a href="https://nextjs.org">Next.js!</a>
 				</h1>
 
 				<p className={styles.description}>
