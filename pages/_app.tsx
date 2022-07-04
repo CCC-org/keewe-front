01ad589a9273f0937ad1c3515a5d13e48011ae5a
akdlsz21 <81629070+akdlsz21@users.noreply.github.com>
2022-07-04 20:23:17 +0900

    [Feat][개발환경설정] mui적용 및 테마설정 (#8)
    * 업데이트
    
    * mui implemented
    
    * MUI styles dependency error fixed
    
    * changed file directory naming
    
    * import naming fixed
    
    Co-authored-by: ysh4296 <dbtmdgns4296@naver.com>

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,30 @@
 import React from 'react';
 
-import '../styles/globals.css';
 import type { AppProps } from 'next/app';
+// import AppPropsProvider from '@store/AppPropsProvider';
+import { ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import { CacheProvider, EmotionCache } from '@emotion/react';
+import theme from '@theme/light';
+import createEmotionCache from 'emotion_cache/createEmotionCache';
 
-function MyApp({ Component, pageProps }: AppProps) {
-	return <Component {...pageProps} />;
+const clientSideEmotionCache = createEmotionCache();
+interface MyAppProps extends AppProps {
+	emotionCache?: EmotionCache;
+}
+
+function MyApp(props: MyAppProps) {
+	const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
+	return (
+		<CacheProvider value={emotionCache}>
+			<ThemeProvider theme={theme}>
+				{/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+				<CssBaseline />
+				<Component {...pageProps} />
+			</ThemeProvider>
+			<p>Hello to me21</p>
+		</CacheProvider>
+	);
 }
 
 export default MyApp;
