9b9f25748a09781795ffca51457c371f97a3e0bc
ysh4296 <dbtmdgns4296@naver.com>
2022-07-05 23:42:47 +0900

    [Fix][개발환경설정] Theme 누락요소 추가 (#9)

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -12,16 +12,17 @@ export default function Document() {
 				type="image/png"
 				sizes="16x16"
 				href="images/favicons/favicon-16x16.png"
-			></link>
+			/>
 			<link
 				rel="icon"
 				type="image/png"
 				sizes="32x32"
 				href="images/favicons/favicon-32x32.png"
-			></link>
-			<link rel="apple-touch-icon" href="images/icons/icon-192x192.png"></link>
-			<meta name="msapplication-TileColor" content="#FF98BA"></meta>
-			<meta name="theme-color" content="#319197"></meta>
+			/>
+			<link rel="stylesheet" href="//cdn.jsdelivr.net/npm/font-applesdgothicneo@1.0/all.min.css"/>
+			<link rel="apple-touch-icon" href="images/icons/icon-192x192.png"/>
+			<meta name="msapplication-TileColor" content="#FF98BA"/>
+			<meta name="theme-color" content="#319197"/>
 			<body>
 				<Main />
 				<NextScript />
