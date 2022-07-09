7a6e6a6b3450ce99fef9fb153a91b259e5a4a17c
ysh4296 <dbtmdgns4296@naver.com>
2022-07-09 19:05:42 +0900

    index.tsx 불필요 코드 삭제

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,8 @@
 import React from 'react';
 
 import type { NextPage } from 'next';
-import { Typography } from '@mui/material';
-import { useTheme } from '@mui/material/styles';
 
 const Home: NextPage = () => {
-	const theme = useTheme();
 	return <></>;
 };
 
