07dc4182d621770338efe2a9d0dc130f55b64d7d
ysh4296 <dbtmdgns4296@naver.com>
2022-07-09 22:30:13 +0900

    미사용 import 제거

--- src/components/Header/PrevArrowSection.tsx
+++ src/components/Header/PrevArrowSection.tsx
@@ -1,13 +1,9 @@
 import React from 'react';
-import SvgIcon from '@mui/material/SvgIcon';
-import IconButton from '@mui/material/Button';
-import { useTheme } from '@mui/material';
 import SVGButton from '@components/Button/SVGButton';
 import Box from '@mui/material/Box';
 import arrow_back from '@icons/arrow_back';
 
 const PrevArrowSection = () => {
-	const theme = useTheme();
 	return (
 		<Box margin="10px 16px 10px 16px">
 			<SVGButton source={arrow_back}></SVGButton>
