af3e27d53badf1443e59be41ba90b8548fda2477
ysh4296 <dbtmdgns4296@naver.com>
2022-07-10 09:05:37 +0900

    새로운 임포트 적용

--- src/components/Header/PrevArrowSection.tsx
+++ src/components/Header/PrevArrowSection.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import SVGButton from '@components/Button/SVGButton';
 import Box from '@mui/material/Box';
-import arrow_back from '@icons/arrow_back';
+import arrow_back_24px from '@icons/arrow_back_24px';
 
 const PrevArrowSection = () => {
 	return (
 		<Box margin="10px 16px 10px 16px">
-			<SVGButton source={arrow_back}></SVGButton>
+			<SVGButton source={arrow_back_24px}></SVGButton>
 		</Box>
 	);
 };
