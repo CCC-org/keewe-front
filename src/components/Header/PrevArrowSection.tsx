fe017511bfa8ae0271092138af99e87e35244097
ysh4296 <dbtmdgns4296@naver.com>
2022-07-10 14:35:55 +0900

    onClick 추가 next/router를 이용한 history back 구현 (#14)
    - onClick 추가 next/router를 이용한 history back 구현

--- src/components/Header/PrevArrowSection.tsx
+++ src/components/Header/PrevArrowSection.tsx
@@ -2,11 +2,14 @@ import React from 'react';
 import SVGButton from '@components/Button/SVGButton';
 import Box from '@mui/material/Box';
 import arrow_back from '@icons/arrow_back';
+import { NextPage } from 'next';
+import { useRouter } from 'next/router';
 
-const PrevArrowSection = () => {
+const PrevArrowSection: NextPage = () => {
+	const router = useRouter();
 	return (
 		<Box margin="10px 16px 10px 16px">
-			<SVGButton source={arrow_back}></SVGButton>
+			<SVGButton source={arrow_back} onClick={() => router.back()}></SVGButton>
 		</Box>
 	);
 };
