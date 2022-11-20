bedd92f559b28e95736f3991b18f7c757c686d23
akdlsz21 <akdlsz21@gmail.com>
2022-11-20 19:33:34 +0900

    installed another package for lottie due to peer conflict

--- pages/oauthKakaoLoading/index.tsx
+++ pages/oauthKakaoLoading/index.tsx
@@ -1,19 +1,18 @@
 import React from 'react';
-import Lottie from 'react-lottie';
+// import Lottie from 'react-lottie';
+import { Player } from '@lottiefiles/react-lottie-player';
 import MainLottie from '../../public/images/lottie/mainLottie.json';
 
 const KakaoOauthRedirectionPage = () => {
 	return (
 		<div>
-			<Lottie
-				options={{
-					loop: true,
-					autoplay: true,
-					animationData: MainLottie,
-					rendererSettings: {
-						preserveAspectRatio: 'xMidYMid slice',
-					},
-				}}
+			<Player
+				src={MainLottie}
+				background="transparent"
+				speed={1}
+				autoplay={true}
+				loop={true}
+				style={{ width: '300px', height: '300px' }}
 			/>
 		</div>
 	);
