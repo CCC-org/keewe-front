e0df7c5bf64f8e32bc7611bc4a2fbf28b610dd3c
cadi <dbtmdgns4296@naver.com>
2023-09-05 18:56:34 +0900

    앱 미설치 시 이동로직 추가

--- pages/link/[type]/[id].tsx
+++ pages/link/[type]/[id].tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import type { NextPage } from 'next';
 import { Player } from '@lottiefiles/react-lottie-player';
 import MainLottie from '../../../public/images/lottie/mainLottie.json';
-import { isMobile } from 'react-device-detect';
+import { isMobile, isAndroid, isIOS } from 'react-device-detect';
 import { useRouter } from 'next/router';
 
 const LinkPage: NextPage = () => {
@@ -10,8 +10,20 @@ const LinkPage: NextPage = () => {
 	const { type, id } = router.query;
 
 	if (isMobile) {
-		if (type !== undefined && id !== undefined)
-			router.push(`keewe:///link/${type}/${id}`);
+		if (type !== undefined && id !== undefined) {
+			try {
+				router.push(`keewe:///link/${type}/${id}`);
+			} catch (e) {
+				if (isAndroid) {
+					router.push(
+						'http://play.google.com/store/apps/details?id=com.akdlsz21.keewe',
+					);
+				}
+				if (isIOS) {
+					router.push('https://apps.apple.com/kr/app/keewe/id6451339222');
+				}
+			}
+		}
 	}
 
 	return (
